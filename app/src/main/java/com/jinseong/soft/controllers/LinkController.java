d8f76cb9edd1241991641460069509a5595176c3
rlawlstjd0077 <rlawlstjd077@gmail.com>
2021-03-27 16:19:16 +0900

    구현: LinkController 'GET /links' 기능을 구현하라

--- app/src/main/java/com/jinseong/soft/controllers/LinkController.java
+++ app/src/main/java/com/jinseong/soft/controllers/LinkController.java
@@ -1,11 +1,26 @@
 package com.jinseong.soft.controllers;
 
+import com.jinseong.soft.application.LinkService;
+import com.jinseong.soft.domain.Link;
+import java.util.List;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * 링크 HTTP 요청 핸들러
  */
 @RestController
+@RequestMapping("/links")
 public class LinkController {
-    
+    private final LinkService linkService;
+
+    public LinkController(LinkService linkService) {
+        this.linkService = linkService;
+    }
+
+    @GetMapping
+    public List<Link> getLinks() {
+        return linkService.getLinks();
+    }
 }
