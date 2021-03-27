4f7528edecaa6e1f39f4112d0c8fe604e2f5d545
rlawlstjd0077 <rlawlstjd077@gmail.com>
2021-03-27 21:25:00 +0900

    구현: LinkController 'GET /links/{id}' 기능을 구현하라
    Not Found 예외 발생시 404 코드를 응답하기 위해서 전역 에러 핸들러와 에러 응답 객체를 추가하였습니다

--- app/src/main/java/com/jinseong/soft/controllers/LinkController.java
+++ app/src/main/java/com/jinseong/soft/controllers/LinkController.java
@@ -5,6 +5,7 @@ import com.jinseong.soft.domain.Link;
 import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -34,6 +35,18 @@ public class LinkController {
     }
 
     /**
+     * 대응되는 식별자의 링크를 응답합니다.
+     *
+     * @param id 링크 식별자
+     * @return 링크
+     */
+    @GetMapping("{id}")
+    public Link getLink(@PathVariable Long id) {
+        return linkService.getLink(id);
+    }
+
+
+    /**
      * 주어진 링크 정보로 새로운 링크를 생성한 뒤 응답합니다.
      *
      * @param link 링크 정보
@@ -44,4 +57,5 @@ public class LinkController {
     public Link createLink(@RequestBody Link link) {
         return linkService.createLink(link);
     }
+
 }
