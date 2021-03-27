63edbd2c2e9cd26e2a6ba7c6abdc179fa28191af
rlawlstjd0077 <rlawlstjd077@gmail.com>
2021-03-27 16:10:16 +0900

    테스트: LinkController 의 링크가 존재하지 않은 경우 '/GET links' 요청 테스트를 추가하라

---
+++ app/src/main/java/com/jinseong/soft/controllers/LinkController.java
@@ -0,0 +1,11 @@
+package com.jinseong.soft.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 링크 HTTP 요청 핸들러
+ */
+@RestController
+public class LinkController {
+    
+}
---
+++ app/src/test/java/com/jinseong/soft/controllers/LinkControllerTest.java
@@ -0,0 +1,59 @@
+package com.jinseong.soft.controllers;
+
+import com.jinseong.soft.application.LinkService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(LinkController.class)
+class LinkControllerTest {
+
+    @MockBean
+    LinkService linkService;
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @BeforeEach
+    void setUp() {
+
+    }
+
+    @Nested
+    @DisplayName("GET /links 요청은")
+    class Describe_GET_links {
+
+        @Nested
+        @DisplayName("링크가 존재하지 않는 경우")
+        class Context_with_no_link {
+            
+            @Test
+            @DisplayName("OK 상태코드와 빈 링크 목록을 응답한다")
+            void It_returns_status_ok_and_empty_list() throws Exception {
+                mockMvc.perform(
+                        get("/links")
+                )
+                        .andExpect(status().isOk())
+                        .andExpect(content().string("[]"));
+
+                verify(linkService).getLinks();
+            }
+        }
+
+        @Nested
+        @DisplayName("링크가 존재하는 경우")
+        class Context_with_links {
+
+        }
+    }
+}
