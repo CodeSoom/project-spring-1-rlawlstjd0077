4c6666069d16555bf7f249a37d92eef2400d6b63
rlawlstjd0077 <rlawlstjd077@gmail.com>
2021-03-27 16:27:26 +0900

    테스트: LinkController 의 링크가 존재하는 경우 '/GET links' 요청 테스트를 추가하라

--- app/src/test/java/com/jinseong/soft/controllers/LinkControllerTest.java
+++ app/src/test/java/com/jinseong/soft/controllers/LinkControllerTest.java
@@ -3,15 +3,19 @@ package com.jinseong.soft.controllers;
 import com.jinseong.soft.LinkTestFixture;
 import com.jinseong.soft.application.LinkService;
 import com.jinseong.soft.domain.Link;
+import java.util.Collections;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.hamcrest.Matchers.containsString;
+import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.verify;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -20,7 +24,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @WebMvcTest(LinkController.class)
 class LinkControllerTest {
 
-    private final Link link = LinkTestFixture.LINK;
+    private final Link LINK = LinkTestFixture.LINK;
 
     @MockBean
     LinkService linkService;
@@ -30,7 +34,7 @@ class LinkControllerTest {
 
     @BeforeEach
     void setUp() {
-
+        Mockito.reset(linkService);
     }
 
     @Nested
@@ -57,7 +61,23 @@ class LinkControllerTest {
         @Nested
         @DisplayName("링크가 존재하는 경우")
         class Context_with_links {
+            @BeforeEach
+            void setUp() {
+                given(linkService.getLinks())
+                        .willReturn(Collections.singletonList(LINK));
+            }
 
+            @Test
+            @DisplayName("OK 상태코드와 링크 목록을 응답한다.")
+            void It_returns_status_ok_and_empty_list() throws Exception {
+                mockMvc.perform(
+                        get("/links")
+                )
+                        .andExpect(status().isOk())
+                        .andExpect(content().string(containsString(LINK.getTitle())));
+
+                verify(linkService).getLinks();
+            }
         }
     }
 }
