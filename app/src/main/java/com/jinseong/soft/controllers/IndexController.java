bbd50012c2285a9830f3a5a8622347095d90328e
rlawlstjd0077 <rlawlstjd077@gmail.com>
2021-04-02 15:55:15 +0900

    추가: 링크 생성 페이지 front end를 추가하라

--- app/src/main/java/com/jinseong/soft/controllers/IndexController.java
+++ app/src/main/java/com/jinseong/soft/controllers/IndexController.java
@@ -11,4 +11,9 @@ public class IndexController {
     public String index(Model model) {
         return "index";
     }
+
+    @GetMapping("create-link")
+    public String createLink() {
+        return "create-link";
+    }
 }
