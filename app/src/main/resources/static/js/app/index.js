81b58d0a90525410c343af5799aed77317082fdc
rlawlstjd0077 <rlawlstjd077@gmail.com>
2021-04-02 19:08:27 +0900

    추가: 링크 목록 페이지 front end 링크 삭제 기능을 추가하라

--- app/src/main/resources/static/js/app/index.js
+++ app/src/main/resources/static/js/app/index.js
@@ -1,40 +1,27 @@
 var main = {
     init : function () {
         var _this = this;
-        $('#btn-save').on('click', function () {
-            _this.save();
-        });
-
         $('#btn-update').on('click', function () {
             _this.update();
         });
 
         $('#btn-delete').on('click', function () {
-            _this.delete();
+            var td = $(this).parent().parent();
+            var id = td.find('#id').attr('value')
+            _this.delete(id);
         });
     },
-    save : function () {
-        var data = {
-            title: $('#title').val(),
-            linkURL: $('#linkURL').val(),
-            description: $('#description').val(),
-            category: $('#category').val(),
-            type: $('#type').val(),
-        };
-
+    delete : function (id) {
         $.ajax({
-            type: 'POST',
-            url: '/links',
-            dataType: 'json',
-            contentType:'application/json; charset=utf-8',
-            data: JSON.stringify(data)
+            type: 'DELETE',
+            url: '/links/' + id
         }).done(function() {
-            alert('글이 등록되었습니다.');
+            alert('글이 삭제 되었습니다.');
             window.location.href = '/';
         }).fail(function (error) {
             alert(JSON.stringify(error));
         });
-    },
+    }
 };
 
 main.init();
