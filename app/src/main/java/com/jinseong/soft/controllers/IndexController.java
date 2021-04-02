6e5fd2cb7991bdfb7426ed0c02b0fa6971d4282e
rlawlstjd0077 <rlawlstjd077@gmail.com>
2021-04-02 17:53:41 +0900

    추가: 링크 목록 페이지 front end 추가

--- app/src/main/java/com/jinseong/soft/controllers/IndexController.java
+++ app/src/main/java/com/jinseong/soft/controllers/IndexController.java
@@ -1,14 +1,21 @@
 package com.jinseong.soft.controllers;
 
+import com.jinseong.soft.application.LinkService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class IndexController {
+    private final LinkService linkService;
+
+    public IndexController(LinkService linkService) {
+        this.linkService = linkService;
+    }
 
     @GetMapping
     public String index(Model model) {
+        model.addAttribute("links", linkService.getLinks());
         return "index";
     }
 
