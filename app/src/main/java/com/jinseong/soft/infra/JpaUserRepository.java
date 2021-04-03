7723ac0b594c134ad93551dd82bbf7e38dcb3c7b
rlawlstjd0077 <rlawlstjd077@gmail.com>
2021-04-03 23:19:11 +0900

    교정: JpaUserRepository의 대상 타입을 교정하라

--- app/src/main/java/com/jinseong/soft/infra/JpaUserRepository.java
+++ app/src/main/java/com/jinseong/soft/infra/JpaUserRepository.java
@@ -1,16 +1,13 @@
 package com.jinseong.soft.infra;
 
-import com.jinseong.soft.domain.Link;
 import com.jinseong.soft.domain.User;
 import com.jinseong.soft.domain.UserRepository;
 import java.util.Optional;
 import org.springframework.data.repository.CrudRepository;
 
 public interface JpaUserRepository
-        extends UserRepository, CrudRepository<Link, Long> {
-    @Override
+        extends UserRepository, CrudRepository<User, Long> {
     User save(User user);
 
-    @Override
     Optional<User> findByIdAndDeletedIsFalse(Long id);
 }
