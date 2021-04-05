38750b69749f36d3a3ee728dc9f0dae4ecc6526d
JSS <rlawlstjd077@gmail.com>
2021-04-05 23:30:24 +0900

    설정: 인증을 구현하기 위해서 Security Config를 추가하라

--- app/src/main/java/com/jinseong/soft/config/WebSecurityConfig.java
+++ app/src/main/java/com/jinseong/soft/config/WebSecurityConfig.java
@@ -1,9 +1,14 @@
 package com.jinseong.soft.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
@@ -11,13 +16,18 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+                .csrf().disable()
+                .headers().frameOptions().disable()
+                .and()
                 .authorizeRequests()
-                .antMatchers("/", "/home").permitAll()
+                .antMatchers("/", "/css/**", "/images/**", "/js/**", "/h2-console/**", "/profile", "favicon.ico", "/resources/**").permitAll()
+                .antMatchers("/css/**", "/js/**", "/img/**", "**/favicon.ico").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
                 .loginPage("/login")
+                .permitAll()
                 .and()
-                .logout().permitAll();
+                .logout().permitAll().logoutSuccessUrl("/");
     }
 }
