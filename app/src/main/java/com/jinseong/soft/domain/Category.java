afd78e4199b29964e33506274bfe3bcaec132a4c
JSS <rlawlstjd077@gmail.com>
2021-04-08 15:20:27 +0900

    추가: Category 도메인을 추가하라

---
+++ app/src/main/java/com/jinseong/soft/domain/Category.java
@@ -0,0 +1,26 @@
+package com.jinseong.soft.domain;
+
+import lombok.*;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+/**
+ * 링크 카테고리 정보
+ * (ex. 개발, 커리어, 디자인, 기획, 마케팅, 금융)
+ */
+@Entity
+@Getter
+@EqualsAndHashCode(of = "id")
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Category {
+    @Id
+    @Generated
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String title;
+}
