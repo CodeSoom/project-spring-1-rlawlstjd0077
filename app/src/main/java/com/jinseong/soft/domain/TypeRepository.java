cd1edf13abdcd1f4db0097e5da31d9287db95b59
JSS <rlawlstjd077@gmail.com>
2021-04-08 17:01:13 +0900

    구현: Type. Category 저장소의 전체조회 기능을 추가하라

--- app/src/main/java/com/jinseong/soft/domain/TypeRepository.java
+++ app/src/main/java/com/jinseong/soft/domain/TypeRepository.java
@@ -1,11 +1,14 @@
 package com.jinseong.soft.domain;
 
+import java.util.List;
 import java.util.Optional;
 
 /**
  * 타입 저장소
  */
 public interface TypeRepository {
+    List<Type> findAll();
+
     Optional<Type> findByTitle(String title);
 
     Type save(Type category);
--- app/src/main/java/com/jinseong/soft/infra/JpaCategoryRepository.java
+++ app/src/main/java/com/jinseong/soft/infra/JpaCategoryRepository.java
@@ -4,10 +4,13 @@ import com.jinseong.soft.domain.Category;
 import com.jinseong.soft.domain.CategoryRepository;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface JpaCategoryRepository
         extends CategoryRepository, CrudRepository<Category, Long> {
+    List<Category> findAll();
+
     Optional<Category> findByTitle(String title);
 
     Category save(Category category);
