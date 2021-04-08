73dfb903e3ed4055560bacbe7b431ea740862305
JSS <rlawlstjd077@gmail.com>
2021-04-08 16:57:58 +0900

    추가: Tag 저장소 추가

---
+++ app/src/main/java/com/jinseong/soft/infra/JpaTagRepository.java
@@ -0,0 +1,19 @@
+package com.jinseong.soft.infra;
+
+import com.jinseong.soft.domain.Link;
+import com.jinseong.soft.domain.LinkRepository;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface JpaTagRepository
+        extends LinkRepository, CrudRepository<Link, Long> {
+    List<Link> findAll();
+
+    Optional<Link> findById(Long id);
+
+    Link save(Link Link);
+
+    void delete(Link Link);
+}
