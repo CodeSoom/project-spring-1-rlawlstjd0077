a4d8491c848ae68125ef2a37271331c193d18167
rlawlstjd0077 <rlawlstjd077@gmail.com>
2021-04-12 00:53:39 +0900

    구현: 유저 로그인시 유저를 환영하도록 하라

--- app/src/main/java/com/jinseong/soft/controllers/IndexController.java
+++ app/src/main/java/com/jinseong/soft/controllers/IndexController.java
@@ -4,13 +4,13 @@ import com.jinseong.soft.application.LinkService;
 import com.jinseong.soft.domain.Link;
 import com.jinseong.soft.domain.Tag;
 import com.jinseong.soft.dto.LinkResponseData;
+import java.security.Principal;
+import java.util.List;
+import java.util.stream.Collectors;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-
-import java.util.List;
-import java.util.stream.Collectors;
 
 @Controller
 public class IndexController {
@@ -21,11 +21,15 @@ public class IndexController {
     }
 
     @GetMapping
-    public String index(Model model) {
+    public String index(Model model, Principal principal) {
         List<LinkResponseData> links = linkService.getLinks()
                 .stream()
                 .map(LinkResponseData::convertLinkToLinkData)
                 .collect(Collectors.toList());
+
+        if (principal != null) {
+            model.addAttribute("user", principal.getName());
+        }
 
         model.addAttribute("links", links);
         return "index";
