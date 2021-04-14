bcc061541785bed725317b03cbeda6e2eb137e2e
JSS <rlawlstjd077@gmail.com>
2021-04-14 17:07:35 +0900

    구현: Link 등록 Frontend UI 의 Category, Type, Tag의 Autocomplete 기능을 구현하라

--- app/src/main/resources/static/js/app/create-link.js
+++ app/src/main/resources/static/js/app/create-link.js
@@ -11,14 +11,52 @@ var main = {
             placeholder: 'Enter a tag',
             secondaryPlaceholder: '+Tag',
         });
+        _this.initCategory();
+        _this.initType();
+        _this.initTag();
     },
+    initCategory : function () {
+        $.get('/links/categories', function(tagArr){
+            var dataArr = {};
+            tagArr.forEach(key => dataArr[key] = null)
+
+            $('#autocomplete-category').autocomplete({
+              data: dataArr,
+            });
+        },'json');
+    },
+
+    initType: function() {
+        $.get('/links/types', function(typeArr){
+            var dataArr = {};
+            typeArr.forEach(key => dataArr[key] = null)
+
+            $('#autocomplete-type').autocomplete({
+              data: dataArr,
+            });
+        },'json');
+    },
+
+    initTag: function() {
+        $.get('/links/tags', function(tagArr){
+            var dataArr = {};
+            tagArr.forEach(key => dataArr[key] = null)
+
+            $('.chips-placeholder').chips({
+                autocompleteOptions: {
+                  data: dataArr,
+                }
+            });
+        },'json');
+    },
+
     save : function () {
         var data = {
             title: $('#title').val(),
             linkURL: $('#linkURL').val(),
             description: $('#description').val(),
-            category: $('#category').val(),
-            type: $('#type').val(),
+            category: $('#autocomplete-category').val(),
+            type: $('#autocomplete-type').val(),
             tags: M.Chips.getInstance($('.chips-placeholder')).chipsData.map(e => e.tag)
         };
 
